f4145bf6ef719835ada956402470130fea174090
Lalu Fatan <lalufatan25@gmail.com>
2026-03-12 09:24:53 +0700

    tugas 3

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,5 +44,8 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
+    }public function products()
+{
+    return $this->hasMany(Product::class);
+}
 }
