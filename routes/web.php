f208ea8309ce3e109e4b467edcdf66709306ac0b
Lalu Fatan <lalufatan25@gmail.com>
2026-04-23 14:05:40 +0700

    Implementasi operasi Create, Read, Update, dan Delete (CRUD) pada antarmuka dan perutean

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Product Page
+    Route::get('/product/export', function () {
+        return "Export Successful! This route is protected by export-product Gate.";
+    })->middleware('can:export-product')->name('product.export');
+    
+    Route::get('/product', [App\Http\Controllers\ProductController::class, 'index'])->name('product.index');
+    Route::post('/product', [App\Http\Controllers\ProductController::class, 'store'])->name('product.store');
+    Route::get('/product/create', [App\Http\Controllers\ProductController::class, 'create'])->name('product.create');
+    Route::get('/product/{id}', [App\Http\Controllers\ProductController::class, 'show'])->name('product.show');
+    Route::put('/product/update/{id}', [App\Http\Controllers\ProductController::class, 'update'])->name('product.update');
+    Route::get('/product/edit/{product}', [App\Http\Controllers\ProductController::class, 'edit'])->name('product.edit');
+    Route::delete('/product/delete/{id}', [App\Http\Controllers\ProductController::class, 'delete'])->name('product.delete');
 });
 
 require __DIR__.'/auth.php';
